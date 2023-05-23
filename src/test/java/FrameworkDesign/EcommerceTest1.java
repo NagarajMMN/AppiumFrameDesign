79663f6f3c6796d430b882e2751a6057b26d55b4
NagarajMMN <nagarajm8300@gmail.com>
2023-05-23 11:39:16 +0530

    parallel execution in click open

--- src/test/java/FrameworkDesign/EcommerceTest1.java
+++ src/test/java/FrameworkDesign/EcommerceTest1.java
@@ -17,8 +17,8 @@ public class EcommerceTest1 extends BaseTest{
     @Test
     public void FillForm_ErrorValidation() throws InterruptedException
     {
-//       driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Nagaraj");
-//        driver.hideKeyboard();
+       driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Nagaraj");
+        driver.hideKeyboard();
         driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
         driver.findElement(By.id("android:id/text1")).click();
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
@@ -31,8 +31,8 @@ public class EcommerceTest1 extends BaseTest{
     @Test
     public void FillForm_PositiveFlow() throws InterruptedException
     {
-        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Rider");
-        driver.hideKeyboard();
+//        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Rider");
+//        driver.hideKeyboard();
         driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
         driver.findElement(By.id("android:id/text1")).click();
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
